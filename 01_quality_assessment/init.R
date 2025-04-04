c5b3ad9df3e09e237610f3749f1f25876e67f1c7
lpantano <lpantano@users.noreply.github.com>
2025-04-04 15:01:21 +0000

    Style code (GHA)

--- 01_quality_assessment/init.R
+++ 01_quality_assessment/init.R
@@ -1,5 +1,5 @@
-nCount_RNA_cutoff = 0
-nFeature_RNA_cutoff = 0
-mitoRatio_cutoff = 0
-riboRatio_cutoff = 0
-Log10GenesPerUMI_cutoff = 0
+nCount_RNA_cutoff <- 0
+nFeature_RNA_cutoff <- 0
+mitoRatio_cutoff <- 0
+riboRatio_cutoff <- 0
+Log10GenesPerUMI_cutoff <- 0
